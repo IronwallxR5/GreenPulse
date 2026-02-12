63e4619583b045cd12c1b7ce85b71a39e4a9e95a
IronwallxR5 <rathipadamr5@gmail.com>
2026-02-12 11:16:28 +0530

    added project routes to server

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
 import 'dotenv/config';
 import App from './app';
-import ImpactRoutes from './routes/impact.routes';
 import AuthRoutes from './routes/auth.routes';
+import ProjectRoutes from './routes/project.routes';
+import ImpactRoutes from './routes/impact.routes';
 
 const app = new App([
   new AuthRoutes(),
+  new ProjectRoutes(),
   new ImpactRoutes()
 ]);
 
